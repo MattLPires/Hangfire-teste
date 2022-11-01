f17873f1caee9d46081b8b5d753654b6e66d5f0c
Matheus Pires <matheus.h98@hotmail.com.br>
2022-11-01 16:28:06 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Empresa.Projeto.Demo.Hangfire.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,33 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Builder;
+
+namespace Empresa.Projeto.Demo.Hangfire
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            GlobalConfiguration.Configuration.UseSqlServerStorage(@"Data Source=PAX00879\SQLEXPRESS01;Initial Catalog=demo;Integrated Security=True;Pooling=False");
+
+            //Fire and forget
+
+            BackgroundJob.Enqueue(() => FireAndForget());
+
+            //Scheduled jobs
+            BackgroundJob.Schedule(() => Console.WriteLine("Teste Schedule"), TimeSpan.FromMilliseconds(10000));
+
+            //Recurring jobs
+            RecurringJob.AddOrUpdate(() => Console.WriteLine("Teste Recurring Job"), Cron.Minutely);
+
+            
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+        }
+
+        public void FireAndForget()
+        {
+            Thread.Sleep(10000);
+            Console.WriteLine("Teste Fire and Forget");
+        }
+    }
+}
